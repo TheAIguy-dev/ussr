1d2805653c7bf52276c59f29c2f7bc4e49d03714
TheAIguy_ <i.oldvillage1313@gmail.com>
2024-06-15 11:50:49 +0300

    Trying to fix writing generic lengths

--- ussr-protocol/src/versions/v1_7_2/packets/login.rs
+++ ussr-protocol/src/versions/v1_7_2/packets/login.rs
@@ -18,8 +18,8 @@ pub mod serverbound {
     packet! {
         #[derive(Debug, Clone, PartialEq, Eq)]
         pub EncryptionResponse {
-            pub shared_secret: Vec<u8> = read_array::<u16, u8>, write_array::<u16, u8>,
-            pub verify_token: Vec<u8> = read_array::<u16, u8>, write_array::<u16, u8>,
+            pub shared_secret: Vec<u8> = read_array::<u16, u8>, |writer, value| write_array::<u16, u8>(writer, value.as_slice()),
+            pub verify_token: Vec<u8> = read_array::<u16, u8>, |writer, value| write_array::<u16, u8>(writer, value.as_slice()),
         }
         const ID = 0x01,
         const DIRECTION = Serverbound,
