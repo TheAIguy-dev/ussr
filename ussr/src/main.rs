4a4b17978e42a1d4d5b54a2f92e500619fa9e6b7
TheAIguy_ <i.oldvillage1313@gmail.com>
2024-06-18 14:52:36 +0300

    test

--- ussr/src/main.rs
+++ ussr/src/main.rs
@@ -1,20 +1,58 @@
-mod init_tracing;
+// mod init_tracing;
 
-use bevy_app::{App, PostStartup, ScheduleRunnerPlugin};
-use tracing::{info, instrument};
-use ussr_net::UssrNetPlugin;
+use std::{io::Cursor, time::Instant};
+
+// use bevy_app::{App, PostStartup, ScheduleRunnerPlugin};
+// use tracing::{info, instrument};
+// use ussr_net::{serialize_packet, UssrNetPlugin};
+use ussr_protocol::{
+    proto::{enums::NextState, packets::handshaking::serverbound::Handshake},
+    Packet,
+};
 
 fn main() {
-    init_tracing::init();
+    // init_tracing::init();
 
-    App::new()
-        .add_plugins(ScheduleRunnerPlugin::default())
-        .add_plugins(UssrNetPlugin)
-        .add_systems(PostStartup, post_startup)
-        .run();
+    // App::new()
+    //     .add_plugins(ScheduleRunnerPlugin::default())
+    //     .add_plugins(UssrNetPlugin)
+    //     .add_systems(PostStartup, post_startup)
+    //     .run();
+
+    const ITERATIONS: usize = 1_000_000;
+
+    let packet: Handshake = Handshake {
+        protocol_version: 765,
+        server_address: "hola!".to_string(),
+        server_port: 25565,
+        next_state: NextState::Status,
+    };
+    let mut buf: Vec<u8> = vec![];
+    packet.write(&mut buf).unwrap();
+    let mut buf: Cursor<Vec<u8>> = Cursor::new(buf.repeat(ITERATIONS));
+
+    let mut packets: Vec<Handshake> = Vec::with_capacity(ITERATIONS);
+    let start: Instant = Instant::now();
+
+    for _ in 0..ITERATIONS {
+        packets.push(Handshake::read(&mut buf).unwrap());
+    }
+
+    println!("{:?}", start.elapsed());
 }
 
-#[instrument(skip_all, level = "trace")]
-fn post_startup() {
-    info!("Done!");
-}
+// #[instrument(skip_all, level = "trace")]
+// fn post_startup() {
+// info!("Done!");
+// }
+
+// let _ = Handshake::read(&mut Cursor::new(&buf));
+// ~1.6s
+
+// let mut buf: Cursor<Vec<u8>> = Cursor::new(buf.repeat(100_000_000));
+// let _ = Handshake::read(&mut buf);
+// ~1.5s
+
+// let buf = buf.repeat(100_000_000);
+// let _ = Handshake::read(&mut &buf[..]);
+// ~1.6s
