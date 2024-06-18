e38009e4d3dd6319c78d3585e533c67a0d40c62b
TheAIguy_ <i.oldvillage1313@gmail.com>
2024-06-18 22:08:53 +0300

    .

--- ussr/src/main.rs
+++ ussr/src/main.rs
@@ -12,28 +12,6 @@ fn main() {
         .add_plugins(UssrNetPlugin)
         .add_systems(PostStartup, post_startup)
         .run();
-
-    // const ITERATIONS: usize = 1_000_000;
-
-    // let packet: Handshake = Handshake {
-    //     protocol_version: 765,
-    //     server_address: "hola!".to_string(),
-    //     server_port: 25565,
-    //     next_state: NextState::Status,
-    // };
-    // let mut buf = vec![];
-    // packet.write(&mut buf).unwrap();
-    // let mut buf = buf.repeat(ITERATIONS);
-
-    // let mut packets: Vec<Handshake> = Vec::with_capacity(ITERATIONS);
-    // let start: Instant = Instant::now();
-
-    // for _ in 0..ITERATIONS {
-    //     packets.push(Handshake::read(&mut Cursor::new(&buf)).unwrap());
-    // }
-
-    // println!("{:?}", start.elapsed());
-    // println!("{:?}", buf.is_empty());
 }
 
 #[instrument(skip_all, level = "trace")]
